b023e34600f57a057e55d9b817d3a4f4b241df03
Carlo Zanella <zanella.carlo@gmail.com>
2013-04-19 16:25:41 +0200

    Adjusted some model parameters

--- demo_binary_labour_choice.f90
+++ demo_binary_labour_choice.f90
@@ -1,6 +1,6 @@
 module demo_binary_labour_choice
     use kinds, only: dp
-    use ncegm, only: ncegm_setup, ncegm_solve, ncegm_model
+    use ncegm, only: ncegm_setup, ncegm_solve, ncegm_model, ncegm_getPolicy_c, ncegm_getPolicy_d, ncegm_getValueFunction, ncegm_getPolicy_aprime
     use grids, only: build_grid
     private
     public :: start_demo_blc
@@ -8,13 +8,13 @@ module demo_binary_labour_choice
 
     ! Model parameters
     real(dp), parameter                         :: tau    = 0.2435_dp
-    real(dp), parameter                         :: delta  = 1.0_dp
+    real(dp), parameter                         :: delta  = 1.75_dp
     real(dp), parameter                         :: roa    = 1.05_dp
-    real(dp), parameter                         :: w      = 5.0_dp
+    real(dp), parameter                         :: w      = 3.0_dp
     real(dp), parameter                         :: beta   = 0.93_dp
 
     ! Nummerical parameters
-    integer, parameter                          :: glen_a = 400
+    integer, parameter                          :: glen_a = 4000
     real(dp), parameter                         :: a_min  = 0
     real(dp), parameter                         :: a_max  = 100
     integer, parameter                          :: glen_d = 2
@@ -23,7 +23,7 @@ module demo_binary_labour_choice
     contains
         subroutine start_demo_blc()
             type(ncegm_model)                   :: model
-            real(dp), dimension(glen_a,1,1)     :: vfinitial
+            real(dp), dimension(glen_a,1,1)     :: vfinitial,choice_d,cf,vf,ap
             integer                             :: i1
 
             ! Allocate required grids
@@ -33,9 +33,9 @@ module demo_binary_labour_choice
             model%a_grid = build_grid(glen_a,a_min,a_max,0) ! Uniform grid over assets a
             model%d_grid = (/ 0.0_dp , 1.0_dp /)
 
-            do i1=1,glen_a
-                vfinitial(i1,1,1) = i1 ! Very naive initial guess for the value function
-            end do
+
+            vfinitial(:,1,1) = u(0.1_dp*model%a_grid,1.0_dp,0.0_dp,0.0_dp)
+
 
             model%F => u
             model%dF => du
@@ -47,6 +47,18 @@ module demo_binary_labour_choice
 
             call ncegm_setup(model)
             call ncegm_solve()
+            vf = ncegm_getValueFunction()
+            cf = ncegm_getPolicy_c()
+            ap = ncegm_getPolicy_aprime()
+            choice_d = ncegm_getPolicy_d()
+            print *, "Value function V(a) & policy functions:"
+            print *, ""
+            print "(a12,a27,a27,a27,a27)", "a", "V(a)", "c*(a)", "a'*(a)", "d*(a)"
+            print "(a12,a27,a27,a27,a27)", "----","----","----","----","----"
+
+            do i1=1,glen_a
+                print *, model%a_grid(i1), vf(i1,1,1), cf(i1,1,1), ap(i1,1,1), choice_d(i1,1,1)
+            end do
 
         end subroutine
 
